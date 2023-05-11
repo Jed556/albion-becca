006b55fc1bcade79685c699cf6d30a63304e39a3
Jed556 <jguiriba11@gmail.com>
2023-05-11 20:00:17 +0800

    feat: Add Albion API player search

---
+++ commands/Albion/player.js
@@ -0,0 +1,51 @@
+const
+    { EmbedBuilder, SlashCommandBuilder, ActivityType } = require('discord.js'),
+    { toError } = require('../../system/functions'),
+    { searchUsers } = require('../../system/albionAPI'),
+    emb = require('../../config/embed.json');
+
+try {
+    module.exports = {
+        data: new SlashCommandBuilder()
+            .setName("search-player")
+            .setDescription("Search Albion player username")
+            .setDefaultMemberPermissions()
+            .setDMPermission(true)
+            .addStringOption(option => option
+                .setName("server")
+                .setDescription("Server to search")
+                .setRequired(true)
+                .addChoices(
+                    { name: "East", value: "east" },
+                    { name: "West", value: "west" },
+                )
+            )
+            .addStringOption(option => option
+                .setName("username")
+                .setDescription("Player's username")
+                .setRequired(true)
+            ),
+        help: "/search-player [server] [username]",
+        cooldown: 3,
+        allowedUIDs: [],
+
+        run: async (client, interaction) => {
+            interaction.deferReply({ ephemeral: true });
+            const { options } = interaction;
+            const server = options.getString("server");
+            const username = options.getString("username");
+
+            users = searchUsers(server, username, false);
+            interaction.editReply({
+                embeds: [new EmbedBuilder()
+                    .setTitle("Search Results")
+                    .setColor(emb.color)
+                    .setTimestamp()
+                    .setFooter({ text: client.user.username, iconURL: client.user.displayAvatarURL({ dynamic: true }) })
+                    .setDescription(`**${users.length}** results found for **${username}** on **${server}**`)
+                    .addField("Results", users.map((user, i) => `**${i + 1}.** [${user.AllianceName}] [${user.GuildName}] ${user.Name}`).join("\n"))
+                ]
+            });
+        }
+    }
+} catch (e) { toError(e, null, 0, false) }
