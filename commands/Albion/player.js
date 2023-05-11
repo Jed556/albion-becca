cb8e6bc125b5dfaad1d5ce7a437bb1d360338973
Jed556 <jguiriba11@gmail.com>
2023-05-11 20:14:02 +0800

    fix: Transpose data to JSON

--- commands/Albion/player.js
+++ commands/Albion/player.js
@@ -45,7 +45,7 @@ try {
                     .setTimestamp()
                     .setFooter({ text: client.user.username, iconURL: client.user.displayAvatarURL({ dynamic: true }) })
                     .setDescription(`**${users.length}** results found for **${username}** on **${server}**`)
-                    .addField("Results", users.map((user, i) => `**${i + 1}.** [${user.AllianceName}] [${user.GuildName}] ${user.Name}`).join("\n"))
+                    .addField("Results", users.toJSON().map((user, i) => `**${i + 1}.** [${user.AllianceName}] [${user.GuildName}] ${user.Name}`).join("\n"))
                 ]
             });
         }
