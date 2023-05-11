ff8e6891332e9963565ec1c4a49f04b8532c1ecb
Jed556 <jguiriba11@gmail.com>
2023-05-11 22:22:20 +0800

    refactor: Optimize code

--- events/database/albionAPI.js
+++ events/database/albionAPI.js
@@ -1,6 +1,7 @@
 const
     { EmbedBuilder } = require('discord.js'),
     { toError } = require('../../system/functions'),
+    { getAPI } = require('../../system/albionAPI'),
     apiConfig = require('../../schemas/Status'),
     mongoose = require('mongoose'),
     axios = require('axios'),
@@ -23,11 +24,10 @@ module.exports = {
     run: async (client) => {
         try {
             // The Albion Killboard API endpoint
-            const server = "east"; // east / west
             const interval = 1; // update interval in minutes
 
             // Build api url
-            const albionApi = `https://gameinfo${server.toLowerCase() == "east" ? "-sgp" : ""}.albiononline.com/api/gameinfo`;
+            const API = getAPI("east");
 
             // The previously retrieved data
             let previousData = [];
@@ -36,8 +36,14 @@ module.exports = {
             const getKillData = async () => {
                 try {
                     // Fetch the latest kill data from the Albion Killboard API
-                    const response = await axios.get(`${albionApi}/events`, { timeout: 0 });
+                    const response = await axios.get(`${API}/events`, { timeout: 0 });
                     const data = response.data;
+
+                    const log = new EmbedBuilder()
+                        .setTitle("Killboard")
+                        .setColor(emb.color)
+                        .setTimestamp()
+                        .setFooter({ text: client.user.username, iconURL: client.user.displayAvatarURL({ dynamic: true }) });
 
                     // Log the latest kill data
                     if (data.length > 0) {
@@ -46,29 +52,17 @@ module.exports = {
                             const alreadyExists = previousData.some((prevKill) => prevKill.EventId === kill.EventId);
 
                             if (!alreadyExists) {
-                                let
-                                    send = 0,
-                                    log = new EmbedBuilder()
-                                        .setTitle("Kill")
-                                        .setColor(emb.color)
-                                        .setTimestamp()
-                                        .setFooter({ text: client.user.username, iconURL: client.user.displayAvatarURL({ dynamic: true }) });
+                                let send = 0;
 
                                 if (kill.Killer.GuildName == "EDGERUNNERS") {
                                     //console.log(`${kill.Killer.Name} killed ${kill.Victim.Name} using ${kill.Killer.Equipment.MainHand.Type} at ${kill.TimeStamp}`);
-                                    log.setDescription(`${kill.Killer.Name} killed ${kill.Victim.Name} using ${kill.Killer.Equipment.MainHand.Type} at ${kill.TimeStamp}`);
-                                    send++;
+                                    send = log.setDescription(`${kill.Killer.Name} killed ${kill.Victim.Name} using ${kill.Killer.Equipment.MainHand.Type} at ${kill.TimeStamp}`);
                                 } else if (kill.Victim.GuildName == "EDGERUNNERS") {
                                     //console.log(`${kill.Victim.Name} was killed by ${kill.Killer.Name} using ${kill.Killer.Equipment.MainHand.Type} at ${kill.TimeStamp}`);
-                                    log.setDescription(`${kill.Victim.Name} was killed by ${kill.Killer.Name} using ${kill.Killer.Equipment.MainHand.Type} at ${kill.TimeStamp}`);
-                                    send++;
+                                    send = log.setDescription(`${kill.Victim.Name} was killed by ${kill.Killer.Name} using ${kill.Killer.Equipment.MainHand.Type} at ${kill.TimeStamp}`);
                                 }
 
-                                if (send) {
-                                    client.users.fetch(OwnerID, false).then((user) => {
-                                        user.send({ embeds: [log] });
-                                    });
-                                }
+                                if (send) client.users.fetch(OwnerID, false).then((user) => user.send({ embeds: [send] }));
                             }
                             previousData.push(kill); // Add the current kill object to previousData (idk but API returns some duplicate data)
                         }
@@ -77,9 +71,9 @@ module.exports = {
                     }
                 } catch (error) {
                     if (error.code === 'ECONNRESET') {
-                        toError("A connection error occurred. Please check your network connection.", "ECONNRESET");
+                        toError("A connection error occurred. Please check your network connection.", "Albion API: ECONNRESET");
                     } else if (error.code === 'ETIMEDOUT') {
-                        toError("Connection timed out. Server did not respond immediately", "ETIMEDOUT");
+                        toError("Connection timed out. Server did not respond immediately", "Albion API: ETIMEDOUT");
                     } else {
                         toError(error);
                     }
@@ -89,7 +83,7 @@ module.exports = {
             // Call the getKillData function to retrieve the latest kill data
             setInterval(getKillData, interval * 1000 * 60);
         } catch (e) {
-            console.log(toError(e, "Database Error"));
+            console.log(toError(e, "Albion API"));
         }
     }
 }
