3e347218aa02d41a1527b8e053a7fb780e82b2d3
Jed556 <jguiriba11@gmail.com>
2023-05-11 22:21:56 +0800

    fix: Fix data overflow

--- commands/Albion/killboard.js
+++ commands/Albion/killboard.js
@@ -26,37 +26,44 @@ try {
         allowedUIDs: [],
 
         run: async (client, interaction) => {
-            interaction.deferReply({ ephemeral: true });
+            const { options } = interaction;
+            const server = options.getString("server");
 
-            const
-                { options } = interaction,
-                server = options.getString("server");
+            // Defer the initial reply
+            await interaction.deferReply();
 
-            API = getAPI(server);
+            // Get the Albion Killboard API URL for the specified server
+            const API = getAPI(server);
 
-            // Fetch the latest kill data from the Albion Killboard API
-            const { data } = await axios.get(`${API}/events`, { timeout: 0 });
+            try {
+                // Fetch the latest kill data from the Albion Killboard API
+                const { data } = await axios.get(`${API}/events`, { timeout: 0 });
 
-            let log = new EmbedBuilder()
-                .setTitle("Killboard")
-                .setColor(emb.color)
-                .setTimestamp()
-                .setFooter({ text: client.user.username, iconURL: client.user.displayAvatarURL({ dynamic: true }) });
+                let log = new EmbedBuilder().setColor(emb.color);
 
-            // Log the latest kill data
-            if (data) {
-                let previousData = [];
-                log.setDescription(data.map((kill) => {
-                    const alreadyExists = previousData.some((prevKill) => prevKill.EventId === kill.EventId);
-                    if (!alreadyExists)
-                        `**${kill.Killer.Name}** killed **${kill.Victim.Name}** using **${kill.Killer.Equipment.MainHand.Type}** (${kill.TimeStamp})\n`;
-                    previousData.push(kill); // Add the current kill object to previousData (idk but API returns some duplicate data)
-                }));
-            } else {
-                log.setColor(emb.errColor).setDescription(`No killboard data found on **${server}**`);
+                // Log the latest kill data
+                if (data) {
+                    let previousData = [];
+                    data.map((kill) => {
+                        const alreadyExists = previousData.some((prevKill) => prevKill.EventId === kill.EventId);
+                        if (!alreadyExists) {
+                            previousData.push(kill); // Add the current kill object to previousData (idk but API returns some duplicate data)
+                            log.setDescription(`**${kill.Killer.Name}** killed **${kill.Victim.Name}** using **${kill.Killer.Equipment.MainHand.Type}** (${kill.TimeStamp})`);
+                            interaction.channel.send({ embeds: [log] });
+                        }
+                    });
+                } else {
+                    log.setColor(emb.errColor).setDescription(`No killboard data found on **${server}**`);
+                    interaction.followUp({ embeds: [log] });
+                }
+            } catch (e) {
+                // Handle any errors that may occur
+                console.error(e);
+                const errorEmbed = new EmbedBuilder()
+                    .setColor(emb.errColor)
+                    .setDescription(`An error occurred while fetching the latest killboard data: ${e.message}`);
+                interaction.followUp({ embeds: [errorEmbed] });
             }
-
-            interaction.reply({ embeds: [log] })
         }
     }
 } catch (e) { toError(e, null, 0, false) }
